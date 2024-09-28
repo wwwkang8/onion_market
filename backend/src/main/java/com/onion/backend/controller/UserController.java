0da76387b44ea0a180fe700fe936b7c919485665
wwwkang8 <wwwkang8@gmail.com>
2024-09-28 13:24:19 +0900

    new: 회원 탈퇴 기능 추가

---
+++ backend/src/main/java/com/onion/backend/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.onion.backend.controller;
+
+import com.onion.backend.entity.User;
+import com.onion.backend.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+@Slf4j
+public class UserController {
+
+    private final UserService userService;
+
+
+    @PostMapping("/create")
+    public ResponseEntity<User> createUser(@RequestParam String username, @RequestParam String password, @RequestParam String email) {
+        User user = userService.createUser(username, password, email);
+        return ResponseEntity.ok(user);
+    }
+
+    @DeleteMapping("/delete/{userId}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long userId) {
+        userService.deleteUser(userId);
+        return ResponseEntity.noContent().build();
+    }
+
+}
